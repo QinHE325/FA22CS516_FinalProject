ecf1bef021049ec24b8beafb2eb3623e88935063
Qin He <heqin@usc.edu>
2022-03-28 16:54:50 -0400

    1

--- mini-amazon-skeleton/app/models/inventory.py
+++ mini-amazon-skeleton/app/models/inventory.py
@@ -1,10 +1,13 @@
+from itertools import product
 from flask import current_app as app
 
 class Inventory:
-    def __init__(self, id, sid, pid, quantity,price,release_date):
+    def __init__(self, id, sid, pid, name,description,quantity,price,release_date):
         self.id = id
         self.sid = sid
         self.pid = pid
+        self.name = name
+        self.description = description 
         self.quantity = quantity
         self.price = price
         self.release_date = release_date
@@ -17,21 +20,51 @@ WHERE id = :id
 ''',
                               id=id)
         return Inventory(*(rows[0])) if rows else None
+    
     @staticmethod
-    def add_inventory(sid,pid,quantity,price):
-        try:
-            rows = app.db.execute("""
-INSERT INTO Inventory(sid, pid, quantity, price)
-VALUES(:sid, :pid, :quantity, :price)
-RETURNING id
+    def product_exists(pid,name):
+        rows = app.db.execute("""
+SELECT name
+FROM Inventory
+WHERE name = :name AND pid = :pid
 """,
-                                  sid=sid,
-                                  pid=pid,
-                                  quantity=quantity, 
-                                  price=price )
-            id = rows[0][0]
-            return Inventory.get(id)
-        except Exception as e:
-            print(str(e))
-            return None
+                              name=name,pid = pid)
+        return len(rows) > 0
+    
+    @staticmethod
+    def add_inventory(self,sid,pid,name,description,quantity,price):
+        if self.product_exists(pid,name):
+            try:
+                rows = app.db.execute("""
+    INSERT INTO Inventory(sid, pid,name,description, quantity, price)
+    VALUES(:sid, :pid, :name,:description, :quantity, :price)
+    RETURNING id
+    """,
+                                    sid=sid,
+                                    pid=pid,
+                                    name = name,
+                                    description = description,
+                                    quantity=quantity, 
+                                    price=price )
+                id = rows[0][0]
+                return Inventory.get(id)
+            except Exception as e:
+                print(str(e))
+                return None
+        # if name exists it will update quantity and description
+        else:
+            try:
+                rows = app.db.execute("""
+    UPDATE Inventory(quantity, description)
+    SET quantity = quantity + :quantity, description = :description
+    RETURNING id
+    """,
+                                    description = description,
+                                    quantity=quantity, 
+                                     )
+                id = rows[0][0]
+                return Inventory.get(id)
+            except Exception as e:
+                print(str(e))
+                return None
          
